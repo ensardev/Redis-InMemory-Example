fcfebab764b88591d5ae5be0449443c81272d620
ensarakkuzey <ensarakkuzey@gmail.com>
2022-04-27 18:27:28 +0300

    Add and enabled InMemory Cache.

---
+++ InMemoryExample.Web/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryExample.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        
+        public CategoryController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
