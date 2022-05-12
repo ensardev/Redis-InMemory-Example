91c8a9529f67e3f22222bb8711e995f472c3d568
ensarakkuzey <ensarakkuzey@gmail.com>
2022-05-12 11:37:09 +0300

    Add cache timing.

--- InMemoryExample.Web/Controllers/CategoryController.cs
+++ InMemoryExample.Web/Controllers/CategoryController.cs
@@ -7,7 +7,7 @@ namespace InMemoryExample.Web.Controllers
     public class CategoryController : Controller
     {
         private IMemoryCache _memoryCache;
-        
+
         public CategoryController(IMemoryCache memoryCache)
         {
             _memoryCache = memoryCache;
@@ -15,14 +15,41 @@ namespace InMemoryExample.Web.Controllers
 
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("date", DateTime.Now.ToString());
+            ////Method 1
+            //if (String.IsNullOrEmpty(_memoryCache.Get<string>("date")))
+            //{
+            //    _memoryCache.Set<string>("date", DateTime.Now.ToString());
+
+            //}
+
+            
+            //Method 2
+            //Zaman ataması yapmak için; 
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            //AbsoluteExpration example
+            //options.AbsoluteExpiration = DateTime.Now.AddSeconds(20);
+
+            //SlidingExpiration example
+            //options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+            //Cache item expire in absolute time with sliding expiration
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+
+            _memoryCache.Set<string>("date", DateTime.Now.ToString(), options);
 
             return View();
         }
 
         public IActionResult GetData()
         {
-            ViewBag.Date =  _memoryCache.Get<string>("date");
+            //_memoryCache.GetOrCreate<string>("date", entry => DateTime.Now.ToString());
+
+            _memoryCache.TryGetValue("date", out string dateCache);
+
+            ViewBag.Date = dateCache;
             return View();
         }
     }
