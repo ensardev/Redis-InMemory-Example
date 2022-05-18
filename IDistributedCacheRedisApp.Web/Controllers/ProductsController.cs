c3e24a55d52131603c6a262221614098b6e99f6e
ensardev <ensarakkuzey@gmail.com>
2022-05-18 21:49:06 +0300

    Redis with basic type data.

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using System;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -14,6 +15,25 @@ namespace IDistributedCacheRedisApp.Web.Controllers
 
         public IActionResult Index()
         {
+            DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions();
+            cacheOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            _distributedCache.SetString("name", "Ensar", cacheOptions);
+
+            return View();
+        }
+        
+        public IActionResult Show()
+        {
+            ViewBag.Name = _distributedCache.GetString("name");
+
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+
             return View();
         }
     }
